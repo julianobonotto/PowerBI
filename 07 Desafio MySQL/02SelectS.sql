76079f5a86a82de10c9c6440c5763643d078f11f
Juliano Kling Bonotto <juliano.bonotto@ibm.com>
2024-09-27 14:31:53 -0300

    Desafio Star Schema

--- 07 Desafio MySQL/02SelectS.sql
+++ 07 Desafio MySQL/02SelectS.sql
@@ -1,3 +1,6 @@
+-- localhost:3306
+-- azure_company
+
 select * from employee;
 
 select * from departament;
@@ -5,3 +8,8 @@ select * from departament;
 select * from works_on;
 
 select * from employee;
+
+select * from dependent;
+
+SELECT d.*, e.Fname FROM departament AS d
+LEFT JOIN employee AS e ON  d.Mgr_ssn = e.Ssn
